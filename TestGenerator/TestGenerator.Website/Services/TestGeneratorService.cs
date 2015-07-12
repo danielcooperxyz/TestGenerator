87daed6a00faadb07aca78c1f9bef876f8a4252b
Daniel Cooper <cooper.daniel45@gmail.com>
2015-07-12 21:48:23 +0100

    Moved TestConstants Separated constants out. Added some TestGeneratorService methods.

--- TestGenerator/TestGenerator.Website/Services/TestGeneratorService.cs
+++ TestGenerator/TestGenerator.Website/Services/TestGeneratorService.cs
@@ -2,11 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TestGenerator.Website.TestParts;
 
 namespace TestGenerator.Website.Services
 {
     public class TestGeneratorService
     {
+        public string CreateTestFileStart(string typeOfClassToTest) 
+        {
+            var fileTop = string.Concat(
+                TestConstants.OpenNamespace,
+                TestConstants.Usings,
+                TestConstants.ClassSummary,
+                TestConstants.ClassDefinition);
 
+            return string.Format(
+                "{0}{1}{2}{3}{{{2}",
+                fileTop,
+                typeOfClassToTest,
+                Environment.NewLine,
+                TestConstants.Indentation);
+        }
+
+        public string CreateTestFileEnd()
+        {
+            return string.Concat(TestConstants.CloseClass + TestConstants.CloseNamespace);
+        }
+
+        public string CreateNullParamTest(string paramType, string parameters)
+        {
+            var testStart = string.Concat(NullParameter.Summary, NullParameter.TestDeclarationStart);
+
+            var testDec = string.Format(
+                "{0}{1}{2}",
+                testStart,
+                paramType,
+                NullParameter.TestDeclarationEnd);
+
+            return testDec;
+        }
     }
 }
---
+++ TestGenerator/TestGenerator.Website/TestParts/TestConstants.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestGenerator.Website.TestParts
+{
+    public static class TestConstants
+    {
+        /* FILE SUMMARY */
+        public const string Indentation = "    ";
+
+        public const string SummaryStart = "/// <summary>";
+
+        public const string SummaryEnd = "/// </summary>";
+
+        public static readonly string OpenNamespace = string.Format(
+            "namespace {0}{{{0}",
+            Environment.NewLine);
+
+        public static readonly string CloseNamespace = "}";
+
+        public static readonly string UsingString = "{0}using System;{1}{0}using System.Collections.Generic;{1}{0}using System.Diagnostics.CodeAnalysis;{1}{0}using System.Linq;{1}" +
+            "{0}using System.Text;{1}{0}using System.Threading.Tasks{1}{0}using Moq;{1}{0}using NUnit.Framework;{1}{1}";
+        
+        public static readonly string Usings = string.Format(
+            UsingString,
+            Indentation,
+            Environment.NewLine);
+
+        public static readonly string ClassSummary = string.Format(
+            "{0}{1}{2}{0}/// Tests for the{2}{0}{3}{2}{0}[ExcludeFromCodeCoverage]{2}",
+            Indentation,
+            SummaryStart,
+            Environment.NewLine,
+            SummaryEnd);
+
+        public static readonly string ClassDefinition = string.Format(
+            "{0}public class {1}{0}{{{1}", 
+            Indentation,
+            Environment.NewLine);
+
+        public static readonly string CloseClass = string.Format(
+            "{0}}}{1}",
+            Indentation,
+            Environment.NewLine);
+    }
+}
