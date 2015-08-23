41e14cba4f7f9bb176039d76508d5ade1f7435c7
Daniel Cooper <cooper.daniel45@gmail.com>
2015-08-23 09:27:30 +0100

    Fixed syntax highlighting
    Included prism files in the solution
    Added string length restrictions to the model

--- TestGenerator/TestGenerator.Website/Models/HomeModel.cs
+++ TestGenerator/TestGenerator.Website/Models/HomeModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,10 @@ namespace TestGenerator.Website.Models
 {
     public class HomeModel
     {
+        [StringLength(250)]
         public string TypeOfClassToTest { get; set; }
 
+        [StringLength(250)]
         public string ClassToTest { get; set; }
 
         public IDictionary<string, string> ClassParameters { get; set; }
